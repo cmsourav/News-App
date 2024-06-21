d6cc4e27d9b6243bb339911a7397d77d4462caff
sourav <sourav@perfectzdigital.com>
2024-06-21 17:24:26 +0530

    implement webview and bottomsheet

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,6 +69,9 @@ dependencies {
     //coil
     implementation(libs.coil.compose)
 
+    //Compose Material3
+    implementation(libs.material3)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/newsapp/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/newsapp/ui/home/HomeScreen.kt
@@ -8,19 +8,34 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.newsapp.model.HomeDataItem
 import com.example.newsapp.ui.components.HomeHeader
 import com.example.newsapp.ui.components.NewsCard
+import com.example.newsapp.ui.components.ScrollableWebview
 import com.example.newsapp.ui.utils.Dimens
 
+@OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun HomeScreen(homeData: List<HomeDataItem>) {
+    val modalBottomSheetState = rememberModalBottomSheetState()
+    var isSheetOpen by rememberSaveable { mutableStateOf(false) }
+    var itemUrl by remember {
+        mutableStateOf("")
+    }
+
     Scaffold(
         topBar = { HomeHeader() },
         content = {
@@ -37,13 +52,21 @@ fun HomeScreen(homeData: List<HomeDataItem>) {
                     contentPadding = PaddingValues(Dimens.dimen16),
                 ) {
                     itemsIndexed(items = homeData, itemContent = { index, item ->
-                        NewsCard(homeDataItem = item) {
+                        NewsCard(homeDataItem = item) { url ->
+                            itemUrl = url
+                            isSheetOpen = true
                         }
                     })
                 }
             }
         },
     )
+    if (isSheetOpen && itemUrl.isNotEmpty()) {
+        ScrollableWebview(sheetState = modalBottomSheetState, url = itemUrl) {
+            isSheetOpen = false
+            itemUrl = ""
+        }
+    }
 }
 
 @Preview(
